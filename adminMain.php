8699f4d913d2b1bf898bb639b49334ef7b48e9ca
msbemh <thdalsehf@naver.com>
2020-05-08 19:45:33 +0900

    관리자페이지 사이드 네비게이션 구현

--- adminMain.php
+++ adminMain.php
@@ -16,12 +16,27 @@ if(!isset($_SESSION['userId'])){
     <?php include './header.php'?>
 </head>
 <body>
-<div class="container" style="min-width:550px; height: 500px;">
-
+<div class="container" style="min-width:550px;">
     <!-- 상단 부분 -->
     <?php include './topPart.php'?>
-    
 </div>
+<div class="container_big" style="height: 100%; margin:20px;">
+    <nav class="side_bar fl">
+        <div class="sign">네비게이션</div>
+        <ul>
+            <li>
+                <a href="/adminMain.php">메인</a>
+            </li>
+            <li>
+                <a href="/adminMain.php">DAY2</a>
+            </li>
+            <li>
+                <a href="/adminMain.php">DAY3</a>
+            </li>
+        </ul>
+    </nav>
+</div>
+
 <script type="text/javascript">
     $(document).on('ready', function(e){
 
